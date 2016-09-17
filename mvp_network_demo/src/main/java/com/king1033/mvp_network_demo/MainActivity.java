c356dfeb32d1f2e0dd4b3c639f795995196a1d7e
King1033 <1786551559@qq.com>
2016-09-18 03:29:35 +0800

    优化MVP的代码

--- mvp_network_demo/src/main/java/com/king1033/mvp_network_demo/MainActivity.java
+++ mvp_network_demo/src/main/java/com/king1033/mvp_network_demo/MainActivity.java
@@ -2,13 +2,26 @@ package com.king1033.mvp_network_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.king1033.mvp_network_demo.presenter.IPresenter;
+import com.king1033.mvp_network_demo.presenter.impl.PresenterImpl;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.gift_list)
+    ListView mListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //处理逻辑，使数据在View界面显示
+        ButterKnife.bind(this);
+
+
     }
 }
